25ea47a672897132f7bb7c59ac3de5fe49f131cf
Aniket Gawande <aniketgaw2005@gmail.com>
2024-09-12 19:31:13 +0530

    update

--- 13while.js
+++ 13while.js
@@ -1,3 +1,22 @@
-while (condition) {
+let index = 0   
+while (index <= 10) {
+    console.log(`value of index is ${index}`);
+    index = index + 2
     
 }
+
+
+let myArray =['falsh','aniket','jay']
+let arr = 0
+while (arr < myArray.length) {
+    console.log(`value is ${myArray[arr]}`);
+    arr = arr + 1
+    
+}
+
+
+let score = 11
+do {
+    console.log(`Score is ${score}`);//  do while loop pheele work fir cond check karta hey
+    score++
+} while (score<=10);
